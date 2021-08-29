a282fe31492464eae6614238f7a158ceb2578fcb
Justin Wash <justin.wash@pm.me>
2021-08-29 10:41:05 -0500

    fix quest not starting on day 1

--- testing/player/Player.gd
+++ testing/player/Player.gd
@@ -21,10 +21,19 @@ func set_input_active(state):
 	can_move = state
 
 func recieve_dialog_signal(event):
+	print("dialog event: ", event)
 	if event == "dialog_started":
 		set_input_active(false)
 	elif event == "dialog_ended":
 		timer.wait_time = 0.5
 		timer.connect("timeout", self, "set_input_active", [true])
 		timer.start()
-
+	elif event.begins_with("set_quest_active"):
+		var cal = get_node("../Calendar")
+		var quest_path = "../Calendar/Days/" + cal.current_day.name+ "/Quests/" + str(event.split(" ")[1])
+		var quest = get_node(quest_path)
+		if (quest):
+			quest.active = true
+			print("set ", quest_path, " active")
+		else:
+			print("couldn't find quest ", quest_path, " to activate")
